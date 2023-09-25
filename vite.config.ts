79ce5165a69e58b35f1914243ae59270b29af462
Victor Stepanov <vix522430@bk.ru>
2023-09-25 11:28:31 +0300

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/picasso-test/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      app: `${path.resolve(__dirname, "./src/app/")}`,
-      entities: `${path.resolve(__dirname, "./src/entities/")}`,
-      pages: path.resolve(__dirname, "./src/pages"),
-      shared: `${path.resolve(__dirname, "./src/shared")}`,
+      '@': path.resolve(__dirname, './src/'),
+      app: `${path.resolve(__dirname, './src/app/')}`,
+      entities: `${path.resolve(__dirname, './src/entities/')}`,
+      pages: path.resolve(__dirname, './src/pages'),
+      shared: `${path.resolve(__dirname, './src/shared')}`,
     },
   },
-});
+})
